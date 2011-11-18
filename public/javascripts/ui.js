9fbd9dce540a89dbfc4b2877cd5f87f045d7cc51
Joshua Holbrook <josh.holbrook@gmail.com>
2011-11-17 16:34:43 -0800

    [ux] An improved dashboard

--- public/javascripts/ui.js
+++ public/javascripts/ui.js
@@ -3,34 +3,61 @@
 var view = {};
 
 view['childHook'] = function () {
-$('.content').append('<pre class="well"></pre>');  
+  $('.content').append('<pre class="well"></pre>');  
 };
-
 
 
 var Hook = require('/hook.js').Hook;
 var hook = new Hook();
 var channel = window.location.hash.substr(1);
+var hooks = {};
 
-hook.on('ready', function(){
-  
+$(function () {
 
-});
+  hook.on('*::hook::ready', function (data) {
+    hooks[data.name] = $('<li id="#'+data.name+'">'+data.name+'</li>');
 
-hook.onAny(function(data){
-  var parts = this.event.split('::');
+    $('#interfaces').append(hooks[data.name]);
+  });
 
-  console.log(data, parts);
+  hook.on('hook::disconnected', function (name) {
+    hooks[name].remove();
+  });
 
-  if (parts[1]==channel) {
-    $('#channel').prepend('<li>' + this.event + '</li>');
+  var truncate = function (str, n) {
+    if (typeof str === "string") {
+      if (str.length < n) {
+        return str;
+      }
+      else {
+        return str.substring(0, n-3)+'...';
+      }
+    }
+    else {
+      return str;
+    }
   }
 
-  if (parts.indexOf('browser') === -1) {
-    $('#pipe').prepend('<li>' + this.event + '</li>');
-  }
+
+  $('#channel').text(channel.length ? ' (Channel `' + channel +'`)' : '');
+
+  hook.onAny(function(data) {
+    var parts = this.event.split('::'),
+        logs = $('#pipe')[0].children[0],
+        maxLength = 30;
+
+    if ( (channel.length && parts[1]==channel)
+      || (!channel.length && parts.indexOf('browser') === -1 ) ) {
+
+      $('#pipe').prepend('<tr><td style="width: 400px;"><strong style="color: #f0f">Event: </strong>' + this.event + '</td><td><strong style="color: #444">Data: </strong>' + truncate(JSON.stringify(data), 80) + '</td></tr>');
+
+      if (logs && (logs.children.length > maxLength)) {
+        logs.removeChild(logs.children[maxLength]);
+      }
+    }
+  });
+
+
+
+  hook.connect();
 });
-
-
-
-hook.connect();
